81c926e92b66289be5bc6fe503cf34077ca95e4b
Dirk Eddelbuettel <edd@debian.org>
2010-12-13 13:25:40 +0000

    version 0.1.0

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+.onLoad <- function (lib, pack) {
+    packageStartupMessage("Now loading:")
+    packageStartupMessage("  RcppDE: C++ Implementation of Differential Evolution Optimisation")
+    packageStartupMessage("  Author: Dirk Eddelbuettel")
+    packageStartupMessage("Based on:")
+    packageStartupMessage("  DEoptim (version 2.0-7): Differential Evolution algorithm in R")
+    packageStartupMessage("  Authors: David Ardia, Katharine Mullen, Brian Peterson and Joshua Ulrich")
+}
